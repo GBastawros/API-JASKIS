7d65fcf2fc035664c58414e9434bf78c1f2f5115
George Bastawros <george.bastawros@outlook.com>
2022-10-05 09:51:30 -0500

    Submit solution with bonus

--- commands.js
+++ commands.js
@@ -302,3 +302,490 @@ db.bounties.updateMany({}, { $set: { captured : true } })
   matchedCount: 3,
   modifiedCount: 3,
   upsertedCount: 0 }
+// Bonous
+// create scanvegers collection
+
+db.createCollection("scavengers")
+{ ok: 1 }
+db.scavengers.insertMany([
+  {
+    "name": "Captain A'Meerkat",
+    "joined": new Date('2011-07-22'),
+    "power": "Thermoregulation",
+    "weapon": "Shovel",
+    "captured": [
+      {
+        "name": "Grim Panda",
+        "species": "Giant Panda",
+        "location": "Temperate forest",
+        "wantedFor": "Eating all the bamboo",
+        "client": "Red wolf",
+        "reward": 5000,
+        "captured": true
+      }
+    ]
+  },
+  {
+    "name": "The Hamster",
+    "joined": new Date('2011-07-22'),
+    "power": "Stealth",
+    "weapon": "Giant rolling ball",
+    "captured": [
+      {
+        "name": "Polarwind",
+        "species": "Polar Bear",
+        "location": "Arctic",
+        "wantedFor": "Not hibernating",
+        "client": "Sabertooth",
+        "reward": 10000,
+        "captured": true
+      }
+    ]
+  },
+  {
+    "name": "Thowl",
+    "joined": new Date('2011-07-22'),
+    "power": "Night vision",
+    "weapon": "Lasers",
+    "captured": [
+      {
+        "name": "Thanoceros",
+        "species": "Rhinoceros",
+        "location": "Grasslands",
+        "wantedFor": "Eating too much grass",
+        "client": "Songbird",
+        "reward": 10000,
+        "captured": true
+      },{
+        "name": "Polarwind",
+        "species": "Polar Bear",
+        "location": "Arctic",
+        "wantedFor": "Not hibernating",
+        "client": "Sabertooth",
+        "reward": 10000,
+        "captured": true
+      }
+    ]
+  },
+  {
+    "name": "Brown Recluse",
+    "joined": new Date('2011-07-22'),
+    "power": "Inciting fear into the heart of enemies",
+    "weapon": "Webs",
+    "captured": [
+      {
+        "name": "Thanoceros",
+        "species": "Rhinoceros",
+        "location": "Grasslands",
+        "wantedFor": "Eating too much grass",
+        "client": "Songbird",
+        "reward": 10000,
+        "captured": true
+      },{
+        "name": "Wrecking Crows",
+        "species": "Crow",
+        "location": "Grasslands",
+        "wantedFor": "Cawing too loudly",
+        "client": "Red wolf",
+        "reward": 40000,
+        "captured": true
+      }
+    ]
+  },
+  {
+    "name": "Falconeye",
+    "joined": new Date('2011-07-22'),
+    "power": "Flight",
+    "captured": [
+      {
+        "name": "Wrecking Crows",
+        "species": "Crow",
+        "location": "Grasslands",
+        "wantedFor": "Cawing too loudly",
+        "client": "Red wolf",
+        "reward": 40000,
+        "captured": true
+      }
+    ]
+  },
+  {
+    "name": "Scarlet Wolf",
+    "joined": new Date('2015-05-01'),
+    "power": "Hunting",
+    "captured": [
+      {
+        "name": "Grim Panda",
+        "species": "Giant Panda",
+        "location": "Temperate forest",
+        "wantedFor": "Eating all the bamboo",
+        "client": "Red wolf",
+        "reward": 5000,
+        "captured": true
+      }
+    ]
+  },
+  {
+    "name": "Black Jaguar",
+    "joined": new Date('2018-04-23'),
+    "power": "Camoflauge",
+    "captured": [
+      {
+        "name": "Grim Panda",
+        "species": "Giant Panda",
+        "location": "Temperate forest",
+        "wantedFor": "Eating all the bamboo",
+        "client": "Red wolf",
+        "reward": 5000,
+        "captured": true
+      },{
+        "name": "Thanoceros",
+        "species": "Rhinoceros",
+        "location": "Grasslands",
+        "wantedFor": "Eating too much grass",
+        "client": "Songbird",
+        "reward": 10000,
+        "captured": true
+      }
+    ]
+  }
+])
+{ acknowledged: true,
+  insertedIds: 
+   { '0': ObjectId("633d1f29e995a18cfd735f94"),
+     '1': ObjectId("633d1f29e995a18cfd735f95"),
+     '2': ObjectId("633d1f29e995a18cfd735f96"),
+     '3': ObjectId("633d1f29e995a18cfd735f97"),
+     '4': ObjectId("633d1f29e995a18cfd735f98"),
+     '5': ObjectId("633d1f29e995a18cfd735f99"),
+     '6': ObjectId("633d1f29e995a18cfd735f9a") } }
+// Find all Scavengers who joined the team after December 31, 2011
+db.scavengers.find({joined:{$gte: new Date(2021-12-31)}})
+{ _id: ObjectId("633d1f29e995a18cfd735f94"),
+  name: 'Captain A\'Meerkat',
+  joined: 2011-07-22T00:00:00.000Z,
+  power: 'Thermoregulation',
+  weapon: 'Shovel',
+  captured: 
+   [ { name: 'Grim Panda',
+       species: 'Giant Panda',
+       location: 'Temperate forest',
+       wantedFor: 'Eating all the bamboo',
+       client: 'Red wolf',
+       reward: 5000,
+       captured: true } ] }
+{ _id: ObjectId("633d1f29e995a18cfd735f95"),
+  name: 'The Hamster',
+  joined: 2011-07-22T00:00:00.000Z,
+  power: 'Stealth',
+  weapon: 'Giant rolling ball',
+  captured: 
+   [ { name: 'Polarwind',
+       species: 'Polar Bear',
+       location: 'Arctic',
+       wantedFor: 'Not hibernating',
+       client: 'Sabertooth',
+       reward: 10000,
+       captured: true } ] }
+{ _id: ObjectId("633d1f29e995a18cfd735f96"),
+  name: 'Thowl',
+  joined: 2011-07-22T00:00:00.000Z,
+  power: 'Night vision',
+  weapon: 'Lasers',
+  captured: 
+   [ { name: 'Thanoceros',
+       species: 'Rhinoceros',
+       location: 'Grasslands',
+       wantedFor: 'Eating too much grass',
+       client: 'Songbird',
+       reward: 10000,
+       captured: true },
+     { name: 'Polarwind',
+       species: 'Polar Bear',
+       location: 'Arctic',
+       wantedFor: 'Not hibernating',
+       client: 'Sabertooth',
+       reward: 10000,
+       captured: true } ] }
+{ _id: ObjectId("633d1f29e995a18cfd735f97"),
+  name: 'Brown Recluse',
+  joined: 2011-07-22T00:00:00.000Z,
+  power: 'Inciting fear into the heart of enemies',
+  weapon: 'Webs',
+  captured: 
+   [ { name: 'Thanoceros',
+       species: 'Rhinoceros',
+       location: 'Grasslands',
+       wantedFor: 'Eating too much grass',
+       client: 'Songbird',
+       reward: 10000,
+       captured: true },
+     { name: 'Wrecking Crows',
+       species: 'Crow',
+       location: 'Grasslands',
+       wantedFor: 'Cawing too loudly',
+       client: 'Red wolf',
+       reward: 40000,
+       captured: true } ] }
+{ _id: ObjectId("633d1f29e995a18cfd735f98"),
+  name: 'Falconeye',
+  joined: 2011-07-22T00:00:00.000Z,
+  power: 'Flight',
+  captured: 
+   [ { name: 'Wrecking Crows',
+       species: 'Crow',
+       location: 'Grasslands',
+       wantedFor: 'Cawing too loudly',
+       client: 'Red wolf',
+       reward: 40000,
+       captured: true } ] }
+{ _id: ObjectId("633d1f29e995a18cfd735f99"),
+  name: 'Scarlet Wolf',
+  joined: 2015-05-01T00:00:00.000Z,
+  power: 'Hunting',
+  captured: 
+   [ { name: 'Grim Panda',
+       species: 'Giant Panda',
+       location: 'Temperate forest',
+       wantedFor: 'Eating all the bamboo',
+       client: 'Red wolf',
+       reward: 5000,
+       captured: true } ] }
+{ _id: ObjectId("633d1f29e995a18cfd735f9a"),
+  name: 'Black Jaguar',
+  joined: 2018-04-23T00:00:00.000Z,
+  power: 'Camoflauge',
+  captured: 
+   [ { name: 'Grim Panda',
+       species: 'Giant Panda',
+       location: 'Temperate forest',
+       wantedFor: 'Eating all the bamboo',
+       client: 'Red wolf',
+       reward: 5000,
+       captured: true },
+     { name: 'Thanoceros',
+       species: 'Rhinoceros',
+       location: 'Grasslands',
+       wantedFor: 'Eating too much grass',
+       client: 'Songbird',
+       reward: 10000,
+       captured: true } ] }
+
+// Find all Scavengers who helped catch 
+db.scavengers.find({"captured.name": "Thanoceros"})
+{ _id: ObjectId("633d1f29e995a18cfd735f96"),
+  name: 'Thowl',
+  joined: 2011-07-22T00:00:00.000Z,
+  power: 'Night vision',
+  weapon: 'Lasers',
+  captured: 
+   [ { name: 'Thanoceros',
+       species: 'Rhinoceros',
+       location: 'Grasslands',
+       wantedFor: 'Eating too much grass',
+       client: 'Songbird',
+       reward: 10000,
+       captured: true },
+     { name: 'Polarwind',
+       species: 'Polar Bear',
+       location: 'Arctic',
+       wantedFor: 'Not hibernating',
+       client: 'Sabertooth',
+       reward: 10000,
+       captured: true } ] }
+{ _id: ObjectId("633d1f29e995a18cfd735f97"),
+  name: 'Brown Recluse',
+  joined: 2011-07-22T00:00:00.000Z,
+  power: 'Inciting fear into the heart of enemies',
+  weapon: 'Webs',
+  captured: 
+   [ { name: 'Thanoceros',
+       species: 'Rhinoceros',
+       location: 'Grasslands',
+       wantedFor: 'Eating too much grass',
+       client: 'Songbird',
+       reward: 10000,
+       captured: true },
+     { name: 'Wrecking Crows',
+       species: 'Crow',
+       location: 'Grasslands',
+       wantedFor: 'Cawing too loudly',
+       client: 'Red wolf',
+       reward: 40000,
+       captured: true } ] }
+{ _id: ObjectId("633d1f29e995a18cfd735f9a"),
+  name: 'Black Jaguar',
+  joined: 2018-04-23T00:00:00.000Z,
+  power: 'Camoflauge',
+  captured: 
+   [ { name: 'Grim Panda',
+       species: 'Giant Panda',
+       location: 'Temperate forest',
+       wantedFor: 'Eating all the bamboo',
+       client: 'Red wolf',
+       reward: 5000,
+       captured: true },
+     { name: 'Thanoceros',
+       species: 'Rhinoceros',
+       location: 'Grasslands',
+       wantedFor: 'Eating too much grass',
+       client: 'Songbird',
+       reward: 10000,
+       captured: true } ] }
+
+// Find all Scavengers who helped catch an animal bounty with a reward greater than $11000.
+db.scavengers.find({"captured.reward":{$gt:1100}})
+{ _id: ObjectId("633d1f29e995a18cfd735f94"),
+  name: 'Captain A\'Meerkat',
+  joined: 2011-07-22T00:00:00.000Z,
+  power: 'Thermoregulation',
+  weapon: 'Shovel',
+  captured: 
+   [ { name: 'Grim Panda',
+       species: 'Giant Panda',
+       location: 'Temperate forest',
+       wantedFor: 'Eating all the bamboo',
+       client: 'Red wolf',
+       reward: 5000,
+       captured: true } ] }
+{ _id: ObjectId("633d1f29e995a18cfd735f95"),
+  name: 'The Hamster',
+  joined: 2011-07-22T00:00:00.000Z,
+  power: 'Stealth',
+  weapon: 'Giant rolling ball',
+  captured: 
+   [ { name: 'Polarwind',
+       species: 'Polar Bear',
+       location: 'Arctic',
+       wantedFor: 'Not hibernating',
+       client: 'Sabertooth',
+       reward: 10000,
+       captured: true } ] }
+{ _id: ObjectId("633d1f29e995a18cfd735f96"),
+  name: 'Thowl',
+  joined: 2011-07-22T00:00:00.000Z,
+  power: 'Night vision',
+  weapon: 'Lasers',
+  captured: 
+   [ { name: 'Thanoceros',
+       species: 'Rhinoceros',
+       location: 'Grasslands',
+       wantedFor: 'Eating too much grass',
+       client: 'Songbird',
+       reward: 10000,
+       captured: true },
+     { name: 'Polarwind',
+       species: 'Polar Bear',
+       location: 'Arctic',
+       wantedFor: 'Not hibernating',
+       client: 'Sabertooth',
+       reward: 10000,
+       captured: true } ] }
+{ _id: ObjectId("633d1f29e995a18cfd735f97"),
+  name: 'Brown Recluse',
+  joined: 2011-07-22T00:00:00.000Z,
+  power: 'Inciting fear into the heart of enemies',
+  weapon: 'Webs',
+  captured: 
+   [ { name: 'Thanoceros',
+       species: 'Rhinoceros',
+       location: 'Grasslands',
+       wantedFor: 'Eating too much grass',
+       client: 'Songbird',
+       reward: 10000,
+       captured: true },
+     { name: 'Wrecking Crows',
+       species: 'Crow',
+       location: 'Grasslands',
+       wantedFor: 'Cawing too loudly',
+       client: 'Red wolf',
+       reward: 40000,
+       captured: true } ] }
+{ _id: ObjectId("633d1f29e995a18cfd735f98"),
+  name: 'Falconeye',
+  joined: 2011-07-22T00:00:00.000Z,
+  power: 'Flight',
+  captured: 
+   [ { name: 'Wrecking Crows',
+       species: 'Crow',
+       location: 'Grasslands',
+       wantedFor: 'Cawing too loudly',
+       client: 'Red wolf',
+       reward: 40000,
+       captured: true } ] }
+{ _id: ObjectId("633d1f29e995a18cfd735f99"),
+  name: 'Scarlet Wolf',
+  joined: 2015-05-01T00:00:00.000Z,
+  power: 'Hunting',
+  captured: 
+   [ { name: 'Grim Panda',
+       species: 'Giant Panda',
+       location: 'Temperate forest',
+       wantedFor: 'Eating all the bamboo',
+       client: 'Red wolf',
+       reward: 5000,
+       captured: true } ] }
+{ _id: ObjectId("633d1f29e995a18cfd735f9a"),
+  name: 'Black Jaguar',
+  joined: 2018-04-23T00:00:00.000Z,
+  power: 'Camoflauge',
+  captured: 
+   [ { name: 'Grim Panda',
+       species: 'Giant Panda',
+       location: 'Temperate forest',
+       wantedFor: 'Eating all the bamboo',
+       client: 'Red wolf',
+       reward: 5000,
+       captured: true },
+     { name: 'Thanoceros',
+       species: 'Rhinoceros',
+       location: 'Grasslands',
+       wantedFor: 'Eating too much grass',
+       client: 'Songbird',
+       reward: 10000,
+       captured: true } ] }
+
+// Find all Scavengers that don't have a weapon.
+db.scavengers.find({weapon: null})
+{ _id: ObjectId("633d1f29e995a18cfd735f98"),
+  name: 'Falconeye',
+  joined: 2011-07-22T00:00:00.000Z,
+  power: 'Flight',
+  captured: 
+   [ { name: 'Wrecking Crows',
+       species: 'Crow',
+       location: 'Grasslands',
+       wantedFor: 'Cawing too loudly',
+       client: 'Red wolf',
+       reward: 40000,
+       captured: true } ] }
+{ _id: ObjectId("633d1f29e995a18cfd735f99"),
+  name: 'Scarlet Wolf',
+  joined: 2015-05-01T00:00:00.000Z,
+  power: 'Hunting',
+  captured: 
+   [ { name: 'Grim Panda',
+       species: 'Giant Panda',
+       location: 'Temperate forest',
+       wantedFor: 'Eating all the bamboo',
+       client: 'Red wolf',
+       reward: 5000,
+       captured: true } ] }
+{ _id: ObjectId("633d1f29e995a18cfd735f9a"),
+  name: 'Black Jaguar',
+  joined: 2018-04-23T00:00:00.000Z,
+  power: 'Camoflauge',
+  captured: 
+   [ { name: 'Grim Panda',
+       species: 'Giant Panda',
+       location: 'Temperate forest',
+       wantedFor: 'Eating all the bamboo',
+       client: 'Red wolf',
+       reward: 5000,
+       captured: true },
+     { name: 'Thanoceros',
+       species: 'Rhinoceros',
+       location: 'Grasslands',
+       wantedFor: 'Eating too much grass',
+       client: 'Songbird',
+       reward: 10000,
+       captured: true } ] }
